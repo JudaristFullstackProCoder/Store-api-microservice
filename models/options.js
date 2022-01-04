7c1fae240d3b52779f322e111fd3ee23f5cd0b85
judaristFullstack <74160484+judaristFullstack@users.noreply.github.com>
2022-01-04 21:06:35 +0100

    create model for product options

---
+++ models/options.js
@@ -0,0 +1,22 @@
+/**
+ * @description the option's schema is used to configure a list of options for a category
+ * for exemple a category can have options for his products; for exemple the category called
+ * phones can have options like : color, width, height, manufacturer etc...
+ * so we would be able to link option(s) to a category and the products of this category
+ * will take the options of their category
+ */
+const mongoose = require("mongoose");
+const Schema = require("mongoose").Schema;
+
+const optionSchema = new Schema({
+    name : {
+        type : Schema.Types.String,
+        required : [true, "the option's name is required"],
+        trim: true
+    }
+});
+
+module.exports = {
+    model : mongoose.model("options", optionSchema),
+    schema : optionSchema
+}
