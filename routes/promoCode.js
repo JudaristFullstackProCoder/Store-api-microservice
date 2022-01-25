b130c7e06e8b2506b8563bc19a5ed0c17dbb8412
judaristFullstack <74160484+judaristFullstack@users.noreply.github.com>
2022-01-25 06:05:00 +0100

    create promo routes

---
+++ routes/promoCode.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const promoCodeController = require("../controllers/promoCode");
+
+router.route("/:id").get(promoCodeController.getPromoCode)
+    .delete(promoCodeController.deletePromoCode).patch(promoCodeController.updatePromoCode);
+router.route("/").post(promoCodeController.createPromoCode);
+
+export default router;
