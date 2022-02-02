b8a21228cbf17a9334f62faff0d70c7e43d400e0
judarist Fullstack <jude22fullstack@gmail.com>
2022-02-02 17:53:48 +0100

    add Check file type

--- libs/MulterUpload.js
+++ libs/MulterUpload.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 module.exports = 
 /**
@@ -6,7 +7,7 @@ module.exports =
  * This function allows you to choose when uploading a file 
  * the destination (subfolder) which must receive this file by passing the name of the subfolder as a parameter.
  */
-function (subDirectory) {
+function (subDirectory, fileType="image") {
     const multer = require("multer");
     // Create the destination directory if it doesn't exist'
 
@@ -16,14 +17,36 @@ function (subDirectory) {
         });
     }
 
-    // File upload
+    // diskStorage
     const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, `uploads/${subDirectory}`)
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + "-" + file.originalname)
-    },
+        destination: (_req, file, cb) => {
+            cb(null, `uploads/${subDirectory}`)
+        },
+        filename: (_req, file, cb) => {
+            cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+        },
+        fileFilter: function(_req, file, cb){
+            checkFileType(file, cb, fileType);
+        }
     });
-    return multer({ storage: storage });
+
+    return multer({ storage: storage, limits: {} });
+}
+
+function checkFileType(file, cb, fileType){
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  if (fileType === "video") {
+    filetypes = /.mp4|.mkv|.avi|.m4v|.ogv|.ogg /
+  }
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if(mimetype && extname){
+    return cb(null,true);
+  } else {
+    cb('Error: This file is not supported ');
+  }
 }
