0448fbf836cc04b26bf994c26759f1218e4f133d
judarist Fullstack <jude22fullstack@gmail.com>
2022-02-03 10:40:50 +0100

    add file validation & fix errors

--- libs/MulterUpload.js
+++ libs/MulterUpload.js
@@ -4,6 +4,7 @@ const path = require('path');
 module.exports = 
 /**
  * @param {String} subDirectory 
+ * @throws {Error}
  * This function allows you to choose when uploading a file 
  * the destination (subfolder) which must receive this file by passing the name of the subfolder as a parameter.
  */
@@ -12,30 +13,33 @@ function (subDirectory, fileType="image") {
     // Create the destination directory if it doesn't exist'
 
     if (!fs.existsSync(`uploads/${subDirectory}`)) {
-        fs.mkdir(`uploads/${subDirectory}`, {recursive : true}, (err) => {
-            console.log(err);
-        });
+        fs.mkdirSync(`uploads/${subDirectory}`, {recursive : false});
     }
 
     // diskStorage
     const storage = multer.diskStorage({
         destination: (_req, file, cb) => {
-            cb(null, `uploads/${subDirectory}`)
+          cb(null, `uploads/${subDirectory}`);
         },
         filename: (_req, file, cb) => {
             cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-        },
-        fileFilter: function(_req, file, cb){
-            checkFileType(file, cb, fileType);
         }
     });
-
-    return multer({ storage: storage, limits: {} });
+    
+    return multer({
+      storage: storage, 
+      limits: {},
+       fileFilter : (_req, file, FileFilterCallback) => {
+          return checkFileType(file, fileType, FileFilterCallback);
+      }
+    });
 }
 
-function checkFileType(file, cb, fileType){
+function checkFileType(file,fileType, FileFilterCallback){
   // Allowed ext
+  
   const filetypes = /jpeg|jpg|png|gif/;
+
   if (fileType === "video") {
     filetypes = /.mp4|.mkv|.avi|.m4v|.ogv|.ogg /
   }
@@ -43,10 +47,11 @@ function checkFileType(file, cb, fileType){
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
   // Check mime
   const mimetype = filetypes.test(file.mimetype);
-
-  if(mimetype && extname){
-    return cb(null,true);
-  } else {
-    cb('Error: This file is not supported ');
+  
+  if(!mimetype || !extname){
+    return FileFilterCallback(new Error('This file is not a valid'), false);
   }
+
+  return FileFilterCallback(null , true);
+
 }
