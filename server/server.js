ed3368ae6d54880c3c459f6ae4cf8936c59c0ae6
judarist Fullstack <jude22fullstack@gmail.com>
2022-02-14 01:38:04 +0100

    fix eslint error

--- server/server.js
+++ server/server.js
@@ -1,84 +1,83 @@
 // Built-in modules imports
 const process = require('process');
-require("dotenv").config({path : require('path').resolve(".env")});
+const resolve = require('path').resolve('.env');
+require('dotenv').config({ path: resolve });
 const cluster = require('cluster');
+const { length } = require('os').cpus();
 
 // Third library imports
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser');
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
-const helmet = require("helmet");
-
-
+const helmet = require('helmet');
 
 // custom modules imports
 const mongoConnection = require('../db/mongoConnect');
 const clientErrorHandler = require('../middlewares/clientErrorHandler');
 const errorLogger = require('../middlewares/errorLogger');
-const notFound = require("../middlewares/notFound");
-const storeRoutes = require("../routes/store");
-const productsRoutes = require("../routes/product");
-const categoryRoutes = require("../routes/category");
-const optionsRoutes = require("../routes/option");
-const promoCodeRoutes = require("../routes/promoCode");
-const uploadRoutes = require("../routes/upload");
+const notFound = require('../middlewares/notFound');
+const storeRoutes = require('../routes/store');
+const productsRoutes = require('../routes/product');
+const categoryRoutes = require('../routes/category');
+const optionsRoutes = require('../routes/option');
+const promoCodeRoutes = require('../routes/promoCode');
+const uploadRoutes = require('../routes/upload');
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(helmet());
 app.use(express.static('../uploads'));
 
 // Handle routes
-app.use("/api/v1/stores", storeRoutes);
-app.use("/api/v1/products", productsRoutes);
-app.use("/api/v1/category", categoryRoutes);
-app.use("/api/v1/options", optionsRoutes);
-app.use("/api/v1/promocodes", promoCodeRoutes);
-app.use("/api/v1/upload", uploadRoutes);
+app.use('/api/v1/stores', storeRoutes);
+app.use('/api/v1/products', productsRoutes);
+app.use('/api/v1/category', categoryRoutes);
+app.use('/api/v1/options', optionsRoutes);
+app.use('/api/v1/promocodes', promoCodeRoutes);
+app.use('/api/v1/upload', uploadRoutes);
 
 // Error handler middleware
 app.use(errorLogger, clientErrorHandler);
 
 // home page
 
-app.all("/", (req, res, next) => {
+app.all('/', (req, res) => {
   res.json({
-    message : "Api Home page"
-  })
-})
+    message: 'Api Home page',
+  });
+});
 
 // Handle not found routes
-app.all("*", notFound);
+app.all('*', notFound);
 
 // start the server if the database connection succed
 // For Master process
 if (cluster.isMaster) {
-    console.log(`Master ${process.pid} is running`);
-    const numCPUs = require('os').cpus().length;
-    // Fork workers.
-    for (let i = 0; i < numCPUs; i++) {
-      cluster.fork();
-      console.log(`Worker ${process.pid} started`);
-    }
-   
-    // This event is firs when worker died
-    cluster.on('exit', (worker, code, signal) => {
-      console.log(`worker ${worker.process.pid} died`);
-    });
-    // For Worker
-  } else {
-    // Workers can share any TCP connection
-    // In this case it is an HTTP server
-    try {
-        mongoConnection(process.env.mongodb);
-        app.listen(process.env.port);
-    }catch(err) {
-        console.log("The server can't start ");
-        console.log(err);
-    }
+  // console.log(`Master ${process.pid} is running`);
+  const numCPUs = length;
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i += 1) {
+    cluster.fork();
+    // console.log(`Worker ${process.pid} started`);
   }
 
+  // This event is firs when worker died
+  cluster.on('exit', (/* worker */) => {
+    // console.log(`worker ${worker.process.pid} died`);
+  });
+  // For Worker
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  try {
+    mongoConnection(process.env.mongodb);
+    app.listen(process.env.port);
+  } catch (err) {
+    console.log(err);
+  }
+}
