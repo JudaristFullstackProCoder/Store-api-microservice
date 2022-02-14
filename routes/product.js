2534acfbd5a8f2fc9823a38b1af7c07134874372
judarist Fullstack <jude22fullstack@gmail.com>
2022-02-14 01:46:19 +0100

    fix eslint warnings

--- routes/product.js
+++ routes/product.js
@@ -1,13 +1,13 @@
-const router = require("express").Router();
-const productController = require("../controllers/product");
+const router = require('express').Router();
+const productController = require('../controllers/product');
 
-router.post("/", productController.createProduct);
-router.route("/:id").get(productController.getProduct)
-    .patch(productController.updateProduct)
-    .delete(productController.deleteProduct);
-    
-router.route("/:id/options")
-    .post(productController.addProductOption)
-    .delete(productController.deleteProductOption);
+router.post('/', productController.createProduct);
+router.route('/:id').get(productController.getProduct)
+  .patch(productController.updateProduct)
+  .delete(productController.deleteProduct);
+
+router.route('/:id/options')
+  .post(productController.addProductOption)
+  .delete(productController.deleteProductOption);
 
 module.exports = router;
