5705e04a2109d8f108d3ffa68222d00762e93677
judarist Fullstack <jude22fullstack@gmail.com>
2022-02-14 01:39:56 +0100

    fix eslint warnings

--- routes/store.js
+++ routes/store.js
@@ -1,10 +1,10 @@
-const router = require("express").Router();
-const storeController = require("../controllers/store");
+const router = require('express').Router();
+const storeController = require('../controllers/store');
 
-router.route("/").post(storeController.createStore);
-router.route("/:id").get(storeController.getStore)
-    .delete(storeController.deleteStore)
-    .patch(storeController.updateStore);
-router.patch("/:id/settings", storeController.updateStoreSettings);
+router.route('/').post(storeController.createStore);
+router.route('/:id').get(storeController.getStore)
+  .delete(storeController.deleteStore)
+  .patch(storeController.updateStore);
+router.patch('/:id/settings', storeController.updateStoreSettings);
 
 module.exports = router;
