da2b21c232fe2c5f3c3e4bd2b263e1bc0da39be4
judarist Fullstack <jude22fullstack@gmail.com>
2022-02-14 01:44:31 +0100

    fix eslint errors and warnings

--- controllers/promoCode.js
+++ controllers/promoCode.js
@@ -1,26 +1,27 @@
-const cm = require("../libs/ApiCrudManager");
-const crudManager = new cm();
-const promoCodeModel = require("../models/promoCode");
+const Cm = require('../libs/ApiCrudManager');
 
-const createPromoCode = function (req, res, next) {
-    return crudManager.create({req:req, res:res, next:next}, promoCodeModel);
-}
+const crudManager = new Cm();
+const promoCodeModel = require('../models/promoCode');
 
-const updatePromoCode = function (req, res, next) {
-    return crudManager.update({req:req, res:res, next:next}, promoCodeModel, []);
-}
+const createPromoCode = function createPromoCod(req, res, next) {
+  return crudManager.create({ req, res, next }, promoCodeModel);
+};
 
-const deletePromoCode = function (req, res, next) {
-    return crudManager.delete({req:req, res:res, next:next}, promoCodeModel);
-}
+const updatePromoCode = function upPromoCod(req, res, next) {
+  return crudManager.update({ req, res, next }, promoCodeModel, []);
+};
 
-const getPromoCode = function (req, res, next) {
-    return crudManager.read({req:req, res:res, next:next}, promoCodeModel);
-}
+const deletePromoCode = function delPromoCod(req, res, next) {
+  return crudManager.delete({ req, res, next }, promoCodeModel);
+};
+
+const getPromoCode = function getPromoCod(req, res, next) {
+  return crudManager.read({ req, res, next }, promoCodeModel);
+};
 
 module.exports = {
-    createPromoCode : createPromoCode,
-    deletePromoCode : deletePromoCode,
-    getPromoCode : getPromoCode,
-    updatePromoCode : updatePromoCode
-}
+  createPromoCode,
+  deletePromoCode,
+  getPromoCode,
+  updatePromoCode,
+};
