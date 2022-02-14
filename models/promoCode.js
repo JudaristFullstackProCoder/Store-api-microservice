76ad96f121b3eac2f437e596f695bb54fceb158b
judarist Fullstack <jude22fullstack@gmail.com>
2022-02-14 01:38:57 +0100

    fix eslint errors

--- models/promoCode.js
+++ models/promoCode.js
@@ -1,35 +1,35 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
 
 /**
  * Promocodes mongoose model
  */
 const promoCodeSchema = new Schema({
-    name : {
-        type : Schema.Types.String,
-        required : true
-    },
-    discount : {
-        type: Schema.Types.Number,
-        required: true
-    },
-    from : {
-        type: Schema.Types.Number, // timestamp GMT
-        required: true,
-        default: parseInt(Date.parse(new Date().toISOString())) // Timestamp GMT
-    },
-    to:{
-        type: Schema.Types.Number, // timestamp GMT
-        required: true
-    },
-    maxuse : {
-        type: Schema.Types.Number,
-        required: true
-    },
-    remaininguse: {
-        type: Schema.Types.Number,
-        required: true
-    }
+  name: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  discount: {
+    type: Schema.Types.Number,
+    required: true,
+  },
+  from: {
+    type: Schema.Types.Number, // timestamp GMT
+    required: true,
+  },
+  to: {
+    type: Schema.Types.Number, // timestamp GMT
+    required: true,
+  },
+  maxuse: {
+    type: Schema.Types.Number,
+    required: true,
+  },
+  remaininguse: {
+    type: Schema.Types.Number,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model("promocodes", promoCodeSchema);
+module.exports = mongoose.model('promocodes', promoCodeSchema);
