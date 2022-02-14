3150f3e4ee5d933d3d3c26a5f859f7d17a1e4db1
judarist Fullstack <jude22fullstack@gmail.com>
2022-02-14 01:46:40 +0100

    fix eslint warnings

--- routes/category.js
+++ routes/category.js
@@ -1,20 +1,20 @@
-const router = require("express").Router();
-const categoryController = require("../controllers/category");
+const router = require('express').Router();
+const categoryController = require('../controllers/category');
 
-router.route("/")
-    .post(categoryController.createCategory);
-router.route("/:id")
-    .get(categoryController.getCategory)
-    .patch(categoryController.updatecategory)
-    .delete(categoryController.deletecategory);
+router.route('/')
+  .post(categoryController.createCategory);
+router.route('/:id')
+  .get(categoryController.getCategory)
+  .patch(categoryController.updatecategory)
+  .delete(categoryController.deletecategory);
 
-router.route("/childs").post(categoryController.createChildCategory);
-router.route("/childs/:id")
-    .delete(categoryController.deleteChildCategory)
-    .get(categoryController.getChildCategory)
-    .patch(categoryController.updateChildCategory);
-router.route("/childs/:id/options")
-    .delete(categoryController.deleteChildCategoryOption)
-    .put(categoryController.addChildCategoryOption);
+router.route('/childs').post(categoryController.createChildCategory);
+router.route('/childs/:id')
+  .delete(categoryController.deleteChildCategory)
+  .get(categoryController.getChildCategory)
+  .patch(categoryController.updateChildCategory);
+router.route('/childs/:id/options')
+  .delete(categoryController.deleteChildCategoryOption)
+  .put(categoryController.addChildCategoryOption);
 
 module.exports = router;
--- routes/option.js
+++ routes/option.js
@@ -1,9 +1,9 @@
-const router = require("express").Router();
-const optionsController = require("../controllers/options");
+const router = require('express').Router();
+const optionsController = require('../controllers/options');
 
-router.route("/:id").get(optionsController.getOption)
-    .patch(optionsController.updateOption)
-    .delete(optionsController.deleteOption);
-router.route("/").post(optionsController.createOption);
+router.route('/:id').get(optionsController.getOption)
+  .patch(optionsController.updateOption)
+  .delete(optionsController.deleteOption);
+router.route('/').post(optionsController.createOption);
 
 module.exports = router;
