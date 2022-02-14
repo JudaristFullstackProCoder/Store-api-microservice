10e67b4e44db935e066261e6a6d1730f9f62b120
judarist Fullstack <jude22fullstack@gmail.com>
2022-02-14 01:48:05 +0100

    fix eslint error

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -6,11 +6,10 @@ const options = {
   serverSelectionTimeoutMS: 5000,
   autoIndex: false, // Don't build indexes
   maxPoolSize: 10, // Maintain up to 10 socket connections
-  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  family: 4 // Use IPv4, skip trying IPv6
-}
+  family: 4, // Use IPv4, skip trying IPv6
+};
 
 module.exports = async function main(url) {
-  return await mongoose.connect(url);
-}
+  return mongoose.connect(url, options);
+};
