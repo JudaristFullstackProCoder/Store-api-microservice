479bfe1d7de61829a606dd75df6354dc2f287deb
Judarist Fullstack Procoder <jude22fullstack@gmail.com>
2022-03-02 20:46:07 +0100

    add an rename routes

--- routes/product.js
+++ routes/product.js
@@ -6,8 +6,9 @@ router.route('/:id').get(productController.getProduct)
   .patch(productController.updateProduct)
   .delete(productController.deleteProduct);
 
-router.route('/:id/options')
-  .post(productController.addProductOption)
-  .delete(productController.deleteProductOption);
+router.route('/:id/option')
+  .post(productController.addProductOption);
+router.route('/:id/option/:option_id').delete(productController.deleteProductOption)
+  .patch(productController.updateProductOption).get(productController.getProductOption);
 
 module.exports = router;
