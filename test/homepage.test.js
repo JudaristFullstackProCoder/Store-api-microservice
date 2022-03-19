24429d7934f5ffb225e8df23afd88c75960e72d2
Judarist Fullstack Procoder <jude22fullstack@gmail.com>
2022-03-19 01:49:23 +0100

    add description

--- test/homepage.test.js
+++ test/homepage.test.js
@@ -6,7 +6,7 @@ const expect = require("chai").expect;
 //Require the dev-dependencies
 let app = require('../server/app');
 
-describe("GET /", () => {
+describe("GET / API homepage", () => {
   it("should return status 200 when trying to get the homepage", async () => {
     const res = await request(app).get("/");
     expect(res.status).to.equal(200);
