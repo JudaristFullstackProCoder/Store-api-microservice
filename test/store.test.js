67ce4678590529709bd3819c3389dc8abaaacfda
Judarist Fullstack Procoder <jude22fullstack@gmail.com>
2022-03-25 12:48:33 +0100

    store test

--- test/store.test.js
+++ test/store.test.js
@@ -30,17 +30,55 @@ it('should return status 201 when we create an store', async () => {
      expect(data).to.have.property('success', true);
      expect(data.data).to.have.property('_id');
      expect(data.data).to.have.property('name', 'store exemple');
+     expect(data.data.settings).to.have.property('_id');
   });
 });
 
+describe('PATCH /api/v1/store/:id', () => {
+     it('it should return 200 status when we updated a store', async () => {
+          let store = await Store.findOne({
+               name: 'store exemple',
+          }).exec();
+          let updatedName = 'Alibaba shopping';
+          const response = await request(app).patch(`/api/v1/store/${store._id}`).send({
+               name: updatedName,
+          });
+          const data = response.body;
+          expect(response.status).to.equal(200);
+          expect(data).to.have.property('data');
+          expect(data).to.have.property('success', true);
+          expect(data.data).to.have.property('_id', store._id.toString());
+          expect(data.data).to.have.property('shopkeeper', store.shopkeeper.toString());
+          expect(data.data).to.have.property('name', updatedName);
+          expect(data.data.settings).to.have.property('_id', store.settings._id.toString());
+     })
+})
+
+describe('GET /api/store/:id', () => {
+     it('it should return status 200 when we retrieve a store informations', async () => {
+          let name = 'Alibaba shopping';
+          let store = await Store.findOne({
+               name,
+          }).exec();
+          let response = await request(app).get(`/api/v1/store/${store._id}`);
+          const data = response.body;
+          expect(response.status).to.equal(200);
+          expect(data).to.have.property('data');
+          expect(data).to.have.property('success', true);
+          expect(data.data).to.have.property('_id', store._id.toString());
+          expect(data.data).to.have.property('shopkeeper', store.shopkeeper.toString());
+          expect(data.data).to.have.property('name', name);
+          expect(data.data.settings).to.have.property('_id', store.settings._id.toString());
+     })
+});
+
 // Delete a store
-describe('DELETE /api/v1/store', () => {
+describe('DELETE /api/v1/store/:id', () => {
      it('should return status 200 when delete store', async () => {
           // Create fake Id for shopkeeper
           let store = await Store.findOne({
-               name: 'store exemple'
+               name: 'Alibaba shopping',
           }).exec();
-          logger.info(store.settings);
           const res = await request(app).delete(`/api/v1/store/${store._id}`);
           const data = res.body;
           expect(res.status).to.equal(200);
@@ -49,5 +87,6 @@ describe('DELETE /api/v1/store', () => {
           expect(data.data).to.have.property('_id', store._id.toString());
           expect(data.data).to.have.property('shopkeeper', store.shopkeeper.toString());
           expect(data.data).to.have.property('name', store.name);
+          expect(data.data.settings).to.has.property('_id', store.settings._id.toString());
      });
 });
