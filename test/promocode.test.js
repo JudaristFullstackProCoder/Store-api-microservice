80edc971676e7b4a6263922552a36daea9858b52
Judarist Fullstack Procoder <jude22fullstack@gmail.com>
2022-03-31 00:47:43 +0100

    write delete promocode test

--- test/promocode.test.js
+++ test/promocode.test.js
@@ -11,9 +11,10 @@ const  Promocode = require('../models/promoCode');
 const expect = require('chai').expect;
 let app = require('../server/app');
 const logger = require('../libs/logger');
+const Promocde = require('../models/promoCode');
 
 before(async () => {
-  await mongooseConnect(mongoose);
+  mongooseConnect(mongoose);
 });
 after(async () => {
   await mongooseDisconnect();
@@ -43,4 +44,28 @@ describe('POST /api/v1/promocode', () => {
           expect(data.data).to.have.property('remaininguse', 1000);
           expect(data.data).to.have.property('name', 'promo785s');
      });
+});
+
+// Delete promocode
+
+describe('DELETE /api/v1/promocode/id', () => {
+  it('should return status 200 when we delete a promocode', async () => {
+    let promocodeName = 'promo785s';
+    let promocode =  await Promocde.findOne({
+      name: promocodeName,
+    }).exec();
+    logger.debug(promocode);
+    const response = await request(app).delete(`/api/v1/promocode/${promocode._id}`);
+    const data = response.body;
+    expect(response.status).to.equal(200);
+    expect(data).to.have.property('data');
+    expect(data).to.have.property('success', true);
+    expect(data.data).to.have.property('_id');
+    expect(data.data).to.have.property('maxuse', promocode.maxuse);
+    expect(data.data).to.have.property('to', promocode.to);
+    expect(data.data).to.have.property('from', promocode.from);
+    expect(data.data).to.have.property('discount', promocode.discount);
+    expect(data.data).to.have.property('remaininguse', promocode.remaininguse);
+    expect(data.data).to.have.property('name', promocode.name);
+  })
 });
