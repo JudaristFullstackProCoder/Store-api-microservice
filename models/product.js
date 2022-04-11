57446bb8afc85c5786a78da635ea939b54d2d9cb
Judarist Fullstack Procoder <jude22fullstack@gmail.com>
2022-04-11 20:33:14 +0100

    update product.js

--- models/product.js
+++ models/product.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const mongoosastic = require('mongoosastic');
 
-const { Schema } = mongoose;
+const connectMongoose = require('../db/mongoConnect');
+
+connectMongoose(mongoose);
 
 /**
  * @description Descrine the strucuture of product's options
@@ -8,16 +11,16 @@ const { Schema } = mongoose;
  * can't have the same option then we need to link product's category options
  * with the product
  */
-const productOptionsSchema = new Schema({
+const productOptionsSchema = new mongoose.Schema({
   // the id of the option
   option: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     required: [false, "option's id is required !"],
     ref: 'options',
   },
   // the value of the option
   value: {
-    type: Schema.Types.Mixed,
+    type: mongoose.Schema.Types.Mixed,
     required: [false, "option's value is required !"],
   },
 });
@@ -25,34 +28,34 @@ const productOptionsSchema = new Schema({
 /**
  * Product composition schema
  */
-const productComposition = new Schema({
+const productComposition = new mongoose.Schema({
   options: [productOptionsSchema],
   image: {
-    type: Schema.Types.Mixed,
+    type: mongoose.Schema.Types.Mixed,
     required: true,
   },
   price: {
-    type: Schema.Types.Number,
+    type: mongoose.Schema.Types.Number,
     required: true,
   },
   product: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     required: [true, 'product id is required for product composition'],
   },
 });
 
-const productSchema = new Schema({
+const productSchema = new mongoose.Schema({
   name: {
-    type: Schema.Types.String,
+    type: mongoose.Schema.Types.String,
     trim: [true, "product's name is required !"],
     required: true,
   },
   price: {
-    type: Schema.Types.Number,
+    type: mongoose.Schema.Types.Number,
     required: [true, "product's price is required !"],
   },
   description: {
-    type: Schema.Types.String,
+    type: mongoose.Schema.Types.String,
     required: [true, "product's description is required !"],
     trim: true,
   },
@@ -62,49 +65,53 @@ const productSchema = new Schema({
     required: false,
   },
   category: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: 'category',
     required: true,
   },
   // if the product has been published already,
   online: {
-    type: Schema.Types.Boolean,
+    type: mongoose.Schema.Types.Boolean,
     required: true,
   },
   // the user who created this product
   shopkeeper: {
-    type: Schema.Types.ObjectId,
-    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    required: false,
   },
   // the is of the store where this product is stored
   store: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     required: true,
     ref: 'store',
   },
   // product's additionals images
   images: {
-    type: Schema.Types.Array,
+    type: mongoose.Schema.Types.Array,
     validate: [function validateImages(arr) { return arr.length <= 5; }, 'maximum number of files reached'],
   },
   image: {
-    type: Schema.Types.Mixed,
+    type: mongoose.Schema.Types.Mixed,
     required: false,
   },
   // presentation video
   pre_video: {
-    type: Schema.Types.Mixed,
+    type: mongoose.Schema.Types.Mixed,
     required: false,
   },
   compositions: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     required: false,
     ref: 'productcompositions',
   },
 });
 
+// mongoosastic
+productSchema.plugin(mongoosastic, {
+});
+
 // Exports
 module.exports = {
-  model: mongoose.model('product', productSchema),
+  model: mongoose.model('products', productSchema),
   ProductComposition: mongoose.model('productcompositions', productComposition),
 };
