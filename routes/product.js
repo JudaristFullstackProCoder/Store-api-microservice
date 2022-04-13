c3c68d5a389f382064d0ea0b08885eecf543bac6
Judarist Fullstack Procoder <jude22fullstack@gmail.com>
2022-04-13 16:09:03 +0100

    add all product composition routes

--- routes/product.js
+++ routes/product.js
@@ -11,4 +11,14 @@ router.route('/:id/option')
 router.route('/:id/option/:optionId').delete(productController.deleteProductOption)
   .patch(productController.updateProductOption).get(productController.getProductOption);
 
+router.route('/:id/variable').post(productController.addProductComposition);
+router.route('/:id/variable/:variableId')
+  .delete(productController.deleteProductComposition)
+  .patch(productController.updateProductComposition);
+router.route('/:id/variable/:variableId').post(productController.addProductCompositionOption);
+router.route('/:id/variable/:variableId/option')
+  .post(productController.addProductCompositionOption)
+  .delete(productController.deleteProductCompositionOption)
+  .patch(productController.updateProductCompositonOption);
+
 module.exports = router;
