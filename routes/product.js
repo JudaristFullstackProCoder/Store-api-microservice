a23a3561827b24fbc7ddf384fabccf5d9c698ab8
Judarist Fullstack Procoder <jude22fullstack@gmail.com>
2022-04-20 07:47:21 +0100

    add \n

--- routes/product.js
+++ routes/product.js
@@ -19,7 +19,8 @@ router.route('/:id/variation/:variationId')
   .delete(productController.deleteProductVariation)
   .patch(productController.updateProductVariation)
   .get(productController.getProductVariation);
-router.route('/:id/variation/:variationId/option').post(productController.addProductVariationOption);
+router.route('/:id/variation/:variationId/option')
+  .post(productController.addProductVariationOption);
 router.route('/:id/variation/:variationId/option/:optionId')
   .delete(productController.deleteProductVariationOption)
   .patch(productController.updateProductVariationOption)
